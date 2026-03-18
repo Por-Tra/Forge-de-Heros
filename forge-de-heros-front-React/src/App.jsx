158b4506cb3a9e7710b539a10bf851a04411ef1b
loazur <clement.bounaix@gmail.com>
2026-03-18 15:27:48 +0100

    Ajout des todos et début création cartes personnages

--- forge-de-heros-front-React/src/App.jsx
+++ forge-de-heros-front-React/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route,  useLocation } from "react-router-dom"
 import Characters from "./pages/Characters"
 import Home from "./pages/Home";
+import CharacterDetail from "./pages/CharacterDetail";
 
 function AppRoutes() {
   const location = useLocation();
@@ -9,7 +10,8 @@ function AppRoutes() {
     <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Home />} />
         <Route path="/characters" element={<Characters />} />
-        <Route path="/parties" element="" />
+        <Route path="/characters/:id" element={<CharacterDetail />} />
+        <Route path="/parties" element={<div>Parties</div>} />
     </Routes>
   )
 }
---
+++ forge-de-heros-front-React/src/pages/CharacterDetail.jsx
@@ -0,0 +1,26 @@
+//import { getCharacter } from "../utils/api";
+
+function CharacterDetail({ characterID })
+{
+    //const data = getCharacter(characterID);
+
+    /* TODO
+        - Afficher toutes les informations du personnages:
+            - Avatar
+            - Stats
+            - Classe
+            - Race
+            - Compétences
+        
+        - Affichage visuel des stats (barre de progression, radar chart, ou autre)
+        - Afficher les groupes auxquels le personnage appartient (CLIQUABLES)
+    */
+
+    return (
+        <p>
+            Test {characterID}
+        </p>
+    )
+}
+
+export default CharacterDetail;
