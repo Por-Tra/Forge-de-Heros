23ea9158a625f8ee7b431401b413c785c3067662
Por-Tra <151206590+Por-Tra@users.noreply.github.com>
2026-03-20 11:21:25 +0100

    ajout du système pour garantir l'accès aux utilisateurs

--- forge-des-heros-API/src/Controller/PartyController.php
+++ forge-des-heros-API/src/Controller/PartyController.php
@@ -10,8 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/party')]
+#[isGranted("ROLE_USER")]
 final class PartyController extends AbstractController
 {
     #[Route(name: 'app_party_index', methods: ['GET'])]
